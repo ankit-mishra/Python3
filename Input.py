c36fcf36d4e7b1950b46e5362fd296fb7bde1882
Ankit Mishra <anki.mishra@gmail.com>
2019-07-06 02:51:50 +0530

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,30 @@
+'''
+Literals
+Python Comments
+Multi-line Comments
+Data Types
+Exploring Types
+Variables
+Variable Names
+Variable Assignment
+Simultaneous Assignment
+A Simple Python Script
+Constants
+Deleting Variables
+Writing a Python Module
+The main() Function
+print() Function
+Named Arguments
+Collecting User Input
+
+'''
+
+
+
+fname = input("Enter First Name")
+fname = input("Enter First Name")
+fname = input("Enter First Name")
+fname = input("Enter First Name")
+fname = input("Enter First Name")
+fname = input("Enter First Name")
+fname = input("Enter First Name")
