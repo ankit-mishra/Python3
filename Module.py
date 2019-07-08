f39fef0fb87f04b031c05db2b94550c48e94e92e
Ankit Mishra <anki.mishra@gmail.com>
2019-07-08 22:06:19 +0530

    Add files via upload

--- Module.py
+++ Module.py
@@ -4,3 +4,9 @@ import Functions
 deptList = {"Telecom","Banking"}
 # Call the function in the Imported module
 Functions._printme(deptList);
+
+'''
+When a module is imported for the first time a .pyc file containing the compiled code
+should be created in a __pycache__ subdirectory of the directory containing the .py file.
+
+'''
